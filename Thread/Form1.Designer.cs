29be8478b2b35b85ef38fe52ff288b6ce6c71a7d
Cyber Hacker <ilgar.hasanzade@gmail.com>
2021-12-26 20:27:37 +0400

    Добавьте файлы проекта.

---
+++ Thread/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Thread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowseFrom = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBrowseTo = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnSuspend = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBrowseFrom
+            // 
+            this.btnBrowseFrom.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnBrowseFrom.Location = new System.Drawing.Point(594, 28);
+            this.btnBrowseFrom.Name = "btnBrowseFrom";
+            this.btnBrowseFrom.Size = new System.Drawing.Size(89, 28);
+            this.btnBrowseFrom.TabIndex = 0;
+            this.btnBrowseFrom.Text = "Browse...";
+            this.btnBrowseFrom.UseVisualStyleBackColor = true;
+            this.btnBrowseFrom.Click += new System.EventHandler(this.btnBrowseFrom_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(64, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(504, 28);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(64, 96);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(504, 28);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "From";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "To";
+            // 
+            // btnBrowseTo
+            // 
+            this.btnBrowseTo.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnBrowseTo.Location = new System.Drawing.Point(594, 96);
+            this.btnBrowseTo.Name = "btnBrowseTo";
+            this.btnBrowseTo.Size = new System.Drawing.Size(89, 28);
+            this.btnBrowseTo.TabIndex = 6;
+            this.btnBrowseTo.Text = "Browse...";
+            this.btnBrowseTo.UseVisualStyleBackColor = true;
+            this.btnBrowseTo.Click += new System.EventHandler(this.btnBrowseTo_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(16, 163);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(552, 28);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCopy.Location = new System.Drawing.Point(594, 163);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(89, 28);
+            this.btnCopy.TabIndex = 8;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnSuspend
+            // 
+            this.btnSuspend.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSuspend.Location = new System.Drawing.Point(143, 223);
+            this.btnSuspend.Name = "btnSuspend";
+            this.btnSuspend.Size = new System.Drawing.Size(89, 28);
+            this.btnSuspend.TabIndex = 9;
+            this.btnSuspend.Text = "Suspend";
+            this.btnSuspend.UseVisualStyleBackColor = true;
+            this.btnSuspend.Click += new System.EventHandler(this.btnSuspend_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnResume.Location = new System.Drawing.Point(359, 223);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(89, 28);
+            this.btnResume.TabIndex = 10;
+            this.btnResume.Text = "Resume";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 271);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnSuspend);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnBrowseTo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnBrowseFrom);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowseFrom;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBrowseTo;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnSuspend;
+        private System.Windows.Forms.Button btnResume;
+    }
+}
+
---
+++ Thread/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Thread
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            Open = new OpenFileDialog();
+            Save = new SaveFileDialog();
+            BackgroundWorker worker = new BackgroundWorker();
+        }
+
+        private void btnBrowseFrom_Click(object sender, EventArgs e)
+        {
+            FileText = "";
+            if(Open.ShowDialog() == DialogResult.OK)
+            {
+                FileText = File.ReadAllText(Open.FileName);
+                textBox1.Text = Open.FileName;
+            }
+        }
+
+        private void btnBrowseTo_Click(object sender, EventArgs e)
+        {
+            if (Open.ShowDialog() == DialogResult.OK)
+            {                
+                textBox2.Text = Open.FileName;
+            }
+        }
+
+        private void Progress(object sender, EventArgs e)
+        {
+            if (progressBar1.Value < 100) progressBar1.Value++;
+            else 
+            { 
+                Timer.Stop();
+                progressBar1.Value = 0;
+                MessageBox.Show("File Copied Successfully");
+            }
+        }
+
+        private void Tick()
+        {
+            Timer = new Timer();
+            Timer.Start();
+            Timer.Enabled = true;
+            Timer.Interval = 1000;
+            progressBar1.Maximum = 100;
+            Timer.Tick += new EventHandler(Progress);
+        }
+
+        private void Copy()
+        {
+            File.WriteAllText(Open.FileName, FileText);
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Copy();
+            Tick();
+        }
+
+        OpenFileDialog Open { get; set; }
+        SaveFileDialog Save { get; set; }
+        Timer Timer { get; set; }
+        string FileText { get; set; }
+
+        private void btnSuspend_Click(object sender, EventArgs e)
+        {
+            Timer.Stop();
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            Timer.Start();
+        }
+    }
+}
